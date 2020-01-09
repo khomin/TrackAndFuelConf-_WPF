74037f74ce3f9b048adb241720971505238c6494
khomin <khominvladimir@yandex.ru>
2020-01-09 16:58:35 +0300

    add panel with errors

--- TrackAndFuelConf/trackerWpfConf/RealTime/RightPanel.xaml.cs
+++ TrackAndFuelConf/trackerWpfConf/RealTime/RightPanel.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace trackerWpfConf.RealTime
+namespace trackerWpfConf.RightPanel
 {
     /// <summary>
     /// Interaction logic for RealTimePanel.xaml
