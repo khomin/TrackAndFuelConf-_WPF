ec35d1dbc7a71af703442b179ff702e656ff43b7
khomin <khominvladimir@yandex.ru>
2020-01-21 19:26:38 +0300

    add input layouts

---
+++ trackerWpfConf/View/Tracker/TrackerInputSettingsItem.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace trackerWpfConf.settings
+{
+    /// <summary>
+    /// Interaction logic for TrackerInputSettingsItem.xaml
+    /// </summary>
+    public partial class TrackerInputSettingsItem : UserControl
+    {
+        public TrackerInputSettingsItem()
+        {
+            InitializeComponent();
+        }
+    }
+}
